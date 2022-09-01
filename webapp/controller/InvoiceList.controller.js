0de72a1bd97f800a0a66639a3d7924d8a026192d
Oliver Sabelfeld <o.sd@tutanota.de>
2022-09-01 14:26:36 +0200

    Step 20: Data Types Number Syntax erlaubt berechnete Felder von mehreren Models, heißen Parts. Währung ist meist im Backend definiert, nicht bei uns, daher definieren.

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,26 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/Fragment"
-], function (Controller, Fragment) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-        onOpenDialog: function () {
-            console.log("HALLO");
-            // create dialog lazily
-            if (!this.pDialog) {
-                this.pDialog = this.loadFragment({
-                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
-                });
-            }
-            this.pDialog.then(function (oDialog) {
-                oDialog.open();
-            });
-        },
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
 
-        onCloseDialog: function () {
-            // note: We don't need to chain to the pDialog promise, since this event-handler
-            // is only called from within the loaded dialog itself.
-            this.byId("helloDialog").close();
-        }
-    });
+	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		}
+	});
 });
