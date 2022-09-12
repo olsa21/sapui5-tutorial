478f58b51460dbd31159d507dcf07518b4b64a86
Oliver Sabelfeld <o.sd@tutanota.de>
2022-09-12 11:09:30 +0200

    Step 28: Integration Test with OPA Testet Integration der App, indem zB Buttons gedrückt werden und die darauffolgende Ereignisse betrachtet.
    Given: iStartMyUIComponent aufrufen
    When: Anwendung in Zustand bringen, in der es getestet werden kann (iPress)
    Then: Enthält Assertions ob das gewollte Ergebnis eintritt (iShouldSee), beenden (iTeardownMyApp)
    
    opaTests.qunit.html -> opaTests.qunit.js -> NavigationJourney.js -> App.js (test, mock) -> HelloPanel.view.xml
    
    Konvention:
    OPA-Tests befinden sich im Ordner webapp/test/integration der Anwendung.
    Verwenden Sie Seiten-Objects  und journeys für die Strukturierung von OPA-Tests.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    });
- });
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict"
+   return Controller.extend("sap.ui.demo.walkthrough.App", {
+       
+       onOpenDialog: function () {
+           this.getOwnerComponent().openHelloDialog();
+       }
+
+   });
+});
